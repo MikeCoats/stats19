9c7d23acef2685d7553f6ba4a9dcef5e4f5817c3
Mike Coats <i.am@mikecoats.com>
2023-10-20 21:13:38 +0100

    Add scripts to download all the stats19 data

---
+++ 2-download-stats19.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+BASE_URL="https://data.dft.gov.uk/road-accidents-safety-data/dft-road-casualty-statistics"
+
+echo "Downloading metadata..."
+curl --silent \
+	"$BASE_URL-road-safety-open-dataset-data-guide-2022.xlsx" \
+	>"./tmp/metadata.xlsx"
+
+for year in $(seq 2018 2022); do
+	for table in collision vehicle casualty; do
+		echo "Downloading $year $table data..."
+		curl --silent \
+			"$BASE_URL-$table-$year.csv" \
+			>"./tmp/$table-$year.csv"
+	done
+done
